47ce298faaefb38184f3ff4443d5c8443259707e
Andrés Osorio <gd_andres98@hotmail.com>
2019-09-14 11:08:22 -0500

    Lets see...

---
+++ app/src/main/java/co/poligran/android_trivia/MainActivity.java
@@ -0,0 +1,25 @@
+package co.poligran.android_trivia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private Button startButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText);
+        startButton = findViewById(R.id.startButton);
+    }
+
+    public void start(View view){
+
+    }
+}
