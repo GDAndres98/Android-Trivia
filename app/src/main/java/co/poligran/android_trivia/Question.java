4fbf8e60edf4429ba42e1cd29c0103458aac88f4
Fernanda Moreno <fernandamoreno494@gmail.com>
2019-09-14 12:27:20 -0500

    Question Object

--- app/src/main/java/co/poligran/android_trivia/Question.java
+++ app/src/main/java/co/poligran/android_trivia/Question.java
@@ -5,11 +5,44 @@ import java.util.Collections;
 
 public class Question {
     private String question;
-    private ArrayList options;
-    private String[] randomOrder;
-    private boolean[] result;
+    private ArrayList<String> options;
+    private ArrayList<String> randomOrder;
 
-    public Question(String q, String c, ArrayList<String> o){
-        question = q.toString();
+    public String getQuestion() {
+        return question;
     }
+
+    public ArrayList<String> getRandomOrder() {
+        return randomOrder;
+    }
+
+    private ArrayList<Integer> result;
+
+    public Question(String q, String c, ArrayList<String> o) {
+        question = q.toString();
+        options = new ArrayList<>();
+        options.add(c);
+        options.addAll(o);
+        randomOrder = new ArrayList<>();
+        result = new ArrayList<>();
+        randomize();
+    }
+
+    public void randomize() {
+        randomOrder.clear();
+        result.clear();
+        for (int i = 0; i < options.size(); i++)
+            result.add(i);
+        Collections.shuffle(result);
+        for (int i = 0; i < options.size(); i++)
+            randomOrder.add(options.get(result.get(i)));
+    }
+
+    public boolean checkQuestion(int i){
+        if(result.get(i)==0)
+            return true;
+        return false;
+    }
+
+
 }
---
+++ app/src/main/java/co/poligran/android_trivia/Trivia.java
@@ -0,0 +1,19 @@
+package co.poligran.android_trivia;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Trivia extends AppCompatActivity {
+    
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trivia);
+
+
+    }
+}
